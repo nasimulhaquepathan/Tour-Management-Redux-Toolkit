f5204a82f1f5d21d4b6b9f392ddc332866adba7d
nasimulhaquepathan <nasimul194@gmail.com>
2023-06-06 01:17:43 +0600

    first commit

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Col, Container, Row } from 'reactstrap';
 
 const AboutPage = () => {
   return (
@@ -7,6 +7,7 @@ const AboutPage = () => {
       <Row>
         <Col>
           <h1>About Us...</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus incidunt architecto non maiores similique obcaecati quam at aliquam dolores voluptatem est, cum numquam temporibus ut delectus, doloribus sint itaque? Fugit.</p>
         </Col>
       </Row>
     </Container>
